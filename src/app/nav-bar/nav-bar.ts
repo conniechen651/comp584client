92fab6688205655437db8301a4916dcd5e240033
Chen <connie.chen.490@my.csun.edu>
2025-09-19 17:30:57 -0700

    Update navbar, UI, and get weather data

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, 
+            MatToolbarModule,
+            MatIconModule,
+            MatButtonModule
+           ],
   templateUrl: './nav-bar.html',
   styleUrls: ['./nav-bar.scss']
 })
--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -9,8 +9,11 @@ import { WeatherData } from '../weather-data';
   styleUrl: './weather.scss'
 })
 export class Weather {
+  forecasts: WeatherData[] = [];
+
   constructor(http: HttpClient){
     http.get<WeatherData[]>('http://localhost:5163/weatherforecast').subscribe(result => {
+      this.forecasts = result;
     });
   }
 }
