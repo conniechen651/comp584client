c5bf30d9e672fa9e19577561341c5cd8612a6c90
conniechen651 <connie91326@gmail.com>
2025-10-24 18:13:21 -0700

    Display country population when click on link on ID.

--- src/app/country-population/country-population.ts
+++ src/app/country-population/country-population.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PopulationData } from './population-data';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-country-population',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './country-population.html',
   styleUrl: './country-population.scss'
 })
-export class CountryPopulation {
-  country!: PopulationData;
-  constructor(http: HttpClient){
-    http.get<PopulationData>(environment.apiUrl + 'api/Countries/population/26q').subscribe(result => {
-      this.country = result;
+export class CountryPopulation implements OnInit {
+  countryPopulation!: PopulationData;
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute){
+    
+  }
+  ngOnInit(): void {
+    let idParam = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get<PopulationData>(`${environment.apiUrl}api/Countries/population/${idParam}`).subscribe(result => {
+      this.countryPopulation = result;
     });
   }
 }
