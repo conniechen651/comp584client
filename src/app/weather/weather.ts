dda7c156c6564d0910d538bed026e6233ab3884d
conniechen651 <connie91326@gmail.com>
2025-11-07 18:11:42 -0800

    Change to using observable when fetching data

--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -1,19 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { WeatherData } from '../weather-data';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './weather.html',
   styleUrl: './weather.scss'
 })
 export class Weather {
-  forecasts: WeatherData[] = [];
+  forecasts$: Observable<WeatherData[]>;
 
   constructor(http: HttpClient){
-    http.get<WeatherData[]>('http://localhost:5163/weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    });
+    this.forecasts$ = http.get<WeatherData[]>('http://localhost:5163/weatherforecast');
   }
 }
